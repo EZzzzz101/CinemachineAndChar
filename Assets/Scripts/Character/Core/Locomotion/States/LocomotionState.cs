d032e99d3472358d4fbcb54e9c1d5644c86d9d25
qq” <1844060405@qq.com>
2026-06-11 18:29:02 +0800

    角色动画驱动状态机

--- Assets/Scripts/Character/Core/Locomotion/States/LocomotionState.cs
+++ Assets/Scripts/Character/Core/Locomotion/States/LocomotionState.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 /// </summary>
 public abstract class LocomotionState : IState
 {
+    protected float _currentSpeed;
+    protected float _speedVelocity;
     protected readonly LocomotionStateMachine Sm;
     protected readonly PlayerController Owner;
 
@@ -26,14 +28,23 @@ public abstract class LocomotionState : IState
         RemoveInputCallbacks();
     }
 
-    /// <summary>子类在这里只订阅自己关心的输入（比如 Idle 关心 Movement.started）</summary>
-    protected virtual void AddInputCallbacks() { }
+    /// <summary>子类在这里只订阅自己关心的输入</summary>
+    protected virtual void AddInputCallbacks(){}
     protected virtual void RemoveInputCallbacks() { }
 
+    //获取目标速度虚函数
+    protected virtual float GetTargetSpeed() => 0f;
+    
     public virtual void Update()
-    {
-        // 每帧设置动画参数
-        Owner.Animator.SetFloat("Movement", Owner.MoveInput.MoveValue.magnitude);
+    {   
+        float targetSpeed =GetTargetSpeed();
+
+         _currentSpeed = Mathf.SmoothDamp(
+            _currentSpeed, targetSpeed,
+            ref _speedVelocity, Owner.SpeedSmoothTime
+        );
+        // 驱动动画
+        Owner.Animator.SetFloat("Movement", _currentSpeed);
         // 转向
         Owner.HandleRotation();
     }
